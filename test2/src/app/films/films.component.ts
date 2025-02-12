71b7a6b731a51f84e5a4fe31b5d9bbd8ba200d0a
Soveliy <34750690+Soveliy@users.noreply.github.com>
2025-02-12 21:00:44 +0300

    feat: 12.02.2025 If, for, some pipes

--- test2/src/app/films/films.component.ts
+++ test2/src/app/films/films.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
-
+import { LowerCasePipe, UpperCasePipe, PercentPipe, CurrencyPipe } from '@angular/common';
 @Component({
   selector: 'app-films',
-  imports: [],
+  imports: [LowerCasePipe,UpperCasePipe,PercentPipe,CurrencyPipe],
   templateUrl: './films.component.html',
   styleUrl: './films.component.scss'
 })
 export class FilmsComponent {
-
+  name = 'title';
+  number = 1000;
+  isLogin = false;
+  filmsList = [{
+    id:0,
+    title: 'Люди в черном',
+    year: 2001,
+  },
+  {
+    id:1,
+    title: 'Люди в черном 2',
+    year: 2002,
+  },
+  {
+    id:2,
+    title: '',
+    year: 2002,
+  },
+]
 }
