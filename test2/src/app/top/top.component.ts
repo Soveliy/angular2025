9aa283af94ec9b7061618172c17991b170b0da9d
Soveliy <34750690+Soveliy@users.noreply.github.com>
2025-02-17 21:31:38 +0300

    feat: add 17.02 genre filters

--- test2/src/app/top/top.component.ts
+++ test2/src/app/top/top.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-top',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './top.component.html',
   styleUrl: './top.component.scss'
 })
@@ -11,7 +13,13 @@ export class TopComponent implements OnInit {
   movies: any[] = [];
   isLoading = true;
   currentPage = 1;
-  totalPages = 0;
+  totalPages = 1;
+  pageInput: number = 1;
+  selecetedGenre = '';
+  genres: string[] = [];
+  filteredMovies: any[] = [];
+
+
   constructor(private http: HttpClient) { }
   ngOnInit(): void {
     this.getTopMovies();
@@ -27,17 +35,47 @@ export class TopComponent implements OnInit {
       .subscribe({
         next: (data) => {
           this.movies = data.items;
-          console.log(data)
+          console.log(data.items)
           this.totalPages = data.totalPages;
           this.isLoading = false;
+          console.log(this.currentPage);
+          this.getGenres();
+          this.applyFilters()
+          console.log(this.genres);
         }
 
       })
   }
 
+  getGenres(): void {
+    const allGenres = new Set<string>();
+    this.movies.forEach((movie) => {
+      movie.genres.forEach((g: any) => allGenres.add(g.genre));
+    })
+    this.genres = Array.from(allGenres);
+  }
+  filterMovies(): void {
+    this.applyFilters();
+  }
+  applyFilters(): void {
+    let movies = [...this.movies];
+    if (this.selecetedGenre ) {
+      movies = movies.filter((movie) =>
+        movie.genres.some((g: any) => g.genre === this.selecetedGenre));
+      this.filteredMovies = movies;
+    } else {
+      this.filteredMovies = this.movies
+    }
+
+  }
+
+
+  // Функции для пагинации
   nextPage(): void {
     if (this.currentPage < this.totalPages) {
       this.currentPage++;
+      this.pageInput++;
+      this.pageInput = this.currentPage
       this.getTopMovies();
     }
 
@@ -46,8 +84,19 @@ export class TopComponent implements OnInit {
   prevPage(): void {
     if (this.currentPage > 1) {
       this.currentPage--;
+      this.pageInput--;
+      this.pageInput = this.currentPage
       this.getTopMovies();
     }
+  }
+
+  goToPage(): void {
+    if (this.pageInput >= 1 && this.pageInput <= this.totalPages) {
+      this.currentPage = this.pageInput;
+
+      this.getTopMovies();
+    }
+
   }
 
 }
